15e13aa4c747b69dc3f4e55243a92a9ab3697d7f
Haley <hemxvc@umsystem.edu>
2021-04-23 16:19:54 -0500

    obstacles generating

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 public class ObstacleGenerator : MonoBehaviour
 {
     public GameObject brickWallPrefab;
+    public GameObject spikePrefab;
+    public GameObject lavaPrefab;
 
-    private float _lastObstaclePosition = 0;
-    private const float ObstacleSpacing = 10f;
+    private int _lastObstaclePosition = 0;
+    private const int ObstacleSpacing = 20;
     private const float TrackWidth = 14f;
     private const float TrackWallX = TrackWidth / 2;
+    private const float TrackLength = 190f;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 5; i++)
+
+        for (int i = ObstacleSpacing; i < TrackLength; i += ObstacleSpacing)
         {
-            GenerateBrickWall();
+            var zLoc = _lastObstaclePosition + ObstacleSpacing;
+            _lastObstaclePosition = zLoc;
+
+            int obstactleType = Random.Range(0, 4);
+
+            switch (obstactleType)
+            {
+                case 0:
+                    GenerateBrickWall(zLoc);
+                    break;
+                case 1:
+                    GenerateJump(zLoc);
+                    break;
+                case 2:
+                    GenerateSpikes(zLoc);
+                    break;
+                case 3:
+                    GenerateLava(zLoc);
+                    break;
+            }
         }
     }
 
@@ -26,30 +49,62 @@ public class ObstacleGenerator : MonoBehaviour
         
     }
 
-    private void GenerateBrickWall()
+    private void GenerateBrickWall(int zLoc)
     {
-        var openingWidth = Random.Range(2f, 7f);
-        var minX = TrackWallX - openingWidth / 2;
-        var openingLoc = Random.Range(-1 * minX, minX);
+        var leftWallLength = Random.Range(0f, 13f);
+        var maxOpening = Mathf.Min(7, TrackWidth - leftWallLength);
+        var opening = Random.Range(2, maxOpening);
+        var rightWallLength = TrackWidth - opening - leftWallLength;
 
-        var rightEnd = openingLoc + openingWidth / 2;
-        var leftEnd = openingLoc - openingWidth / 2;
-        var rightCenter = (rightEnd + TrackWidth/2) / 2;
-        var leftCenter = (leftEnd - TrackWidth/2) / 2;
+        var rightCenter = TrackWallX - rightWallLength / 2;
+        var leftCenter = -1*(TrackWallX - leftWallLength / 2);
 
-        var zLoc = _lastObstaclePosition + ObstacleSpacing;
-        _lastObstaclePosition = zLoc;
+        if (leftWallLength > 0.5)
+        {
+            var leftWall = Instantiate(brickWallPrefab);
+            leftWall.transform.localScale = new Vector3(leftWallLength, 4, 1);
+            leftWall.transform.position = new Vector3(leftCenter, 2, zLoc);
+        }
+        
+        if (rightWallLength > 0.5)
+        {
+            var rightWall = Instantiate(brickWallPrefab);
+            rightWall.transform.localScale = new Vector3(rightWallLength, 4, 1);
+            rightWall.transform.position = new Vector3(rightCenter, 2, zLoc);
+        }
+    }
 
-        var leftWall = Instantiate(brickWallPrefab);
-        var leftLength = leftEnd - TrackWidth/2;
-        leftWall.transform.localScale = new Vector3(leftLength, 4, 1);
-        leftWall.transform.position = new Vector3(leftCenter, 2, zLoc);
+    private void GenerateJump(int zLoc)
+    {
+        var height = Random.Range(2f, 5f);
 
-        var rightWall = Instantiate(brickWallPrefab);
-        var rightLength = TrackWidth / 2 - rightEnd;
-        rightWall.transform.localScale = new Vector3(rightLength, 4, 1);
-        rightWall.transform.position = new Vector3(rightCenter, 2, zLoc);
+        var wall = Instantiate(brickWallPrefab);
+        wall.transform.localScale = new Vector3(TrackWidth, height, 1);
+        wall.transform.position = new Vector3(0, 0, zLoc);
+    }
 
+    private void GenerateSpikes(int zLoc)
+    {
+        var opening = Random.Range(-3, 2);
+        opening = opening * 2; // number msut be even
 
+        for (int i = -6; i <= 6; i += 2)
+        {
+            if (i != opening && i != opening + 2)
+            {
+                var spike = Instantiate(spikePrefab);
+                spike.transform.position = new Vector3(i, 2.25f, zLoc);
+            }
+        }
+    }
+
+    private void GenerateLava(int zLoc)
+    {
+        var bridgeLoc = Random.Range(-4f, 4f);
+
+        var lavaObj = Instantiate(lavaPrefab);
+        lavaObj.transform.position = new Vector3(0, 0.26f, zLoc);
+        var bridge = lavaPrefab.transform.GetChild(0).gameObject;
+        bridge.transform.position = new Vector3(bridgeLoc, 1.75f, 10);
     }
 }
